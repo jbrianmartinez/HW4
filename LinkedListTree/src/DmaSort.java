f38460a078d721677b624a4c84468ea9385c9941
Brian Martinez <marjosep@msudenver.edu>
2016-11-23 22:18:04 -0700

    update LinkedListTree code

--- LinkedListTree/src/DmaSort.java
+++ LinkedListTree/src/DmaSort.java
@@ -6,20 +6,21 @@
 
 import java.io.FileReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  *
  * @author Brian Martinez
  */
 public class DmaSort {
-    private class DMA
+    private static class DMA
     {
         int AreaCode;
         String Region;
         String State;
     }
     
-    public void main(String[] args)
+    public static void main(String[] args)
     {
         LinkedListTree llt = new LinkedListTree();
         String filename = "dma.txt", str = "";
@@ -27,12 +28,65 @@ public class DmaSort {
         try
         {
             Scanner inFile = new Scanner(new FileReader(filename));
+            
+            // using area code as key
+            long start = System.currentTimeMillis();
+            
             while (inFile.hasNext())
             {
                 str = inFile.nextLine();
+                StringTokenizer st = new StringTokenizer(str, ",");
+        
                 DMA dma = new DMA();
-                
+                while (st.hasMoreTokens())
+                {
+                    dma.AreaCode = Integer.parseInt(st.nextToken());
+                    dma.Region = st.nextToken();
+                    dma.State = st.nextToken();
+                    llt.insert(dma.AreaCode, dma);
+                }
             }
+            long time = System.currentTimeMillis()- start;
+            
+            System.out.println("Using area code as key...");
+            System.out.println("Load complete, execution time: " + time + " ms");
+            System.out.println("Total nodes inserted: " + llt.countNodes());
+            System.out.println("Number of balances:   " + llt.GetBalanceCount());
+            System.out.println("Nodes, preorder traversal:");
+            llt.preorder();
+            System.out.println("\nNodes, inorder traversal:");
+            llt.inorder();
+            
+            // using region as key
+            inFile = new Scanner(new FileReader(filename));
+            llt.clear();
+            
+            start = System.currentTimeMillis();
+            
+            while (inFile.hasNext())
+            {
+                str = inFile.nextLine();
+                StringTokenizer st = new StringTokenizer(str, ",");
+        
+                DMA dma = new DMA();
+                while (st.hasMoreTokens())
+                {
+                    dma.AreaCode = Integer.parseInt(st.nextToken());
+                    dma.Region = st.nextToken();
+                    dma.State = st.nextToken();
+                    llt.insert(dma.Region, dma);
+                }
+            }
+            time = System.currentTimeMillis()- start;
+            
+            System.out.println("\n\nUsing region as key...");
+            System.out.println("Load complete, execution time: " + time + " ms");
+            System.out.println("Total nodes inserted: " + llt.countNodes());
+            System.out.println("Number of balances:   " + llt.GetBalanceCount());
+            System.out.println("Nodes, preorder traversal:");
+            llt.preorder();
+            System.out.println("\nNodes, inorder traversal:");
+            llt.inorder();
         }
         catch (Exception ex)
         {
--- LinkedListTree/src/LinkedListTree.java
+++ LinkedListTree/src/LinkedListTree.java
@@ -1,118 +1,213 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Java Program to Implement Self Balancing Binary Search Tree
  */
+ 
+ import java.util.Scanner;
+ 
+ /* Class Node */
+ 
+ /* Class LinkedListTree */
+ public class LinkedListTree<Key extends Comparable<Key>, Value> {
+     private Node root;     
+     private int balanceCnt = 0;
+ 
+     private class Node {  
+        Node left, right;
+        Key key;
+        Value value;
+        int height;
 
+        /* Constructor */
+        public Node() {
+            left = null;
+            right = null;
+            key = null;
+            value = null;
+            height = 0;
+        }
 
-/**
- *
- * @author Brian Martinez
- */
-public class LinkedListTree<Key extends Comparable<Key>, Value> {
-    private Node root;             // root of BST
-
-    private class Node {
-        private Key key;           // sorted by key
-        private Value val;         // associated data
-        private Node left, right;  // left and right subtrees
-        private int size;          // number of nodes in subtree
-
-        public Node(Key key, Value val, int size) {
+        /* Constructor */
+        public Node(Key key, Value val, int height) {
             this.key = key;
-            this.val = val;
-            this.size = size;
-        }
+            this.value = val;
+            this.height = height;
+        }     
     }
-    
-    public int size()
-    {
-        return size(root);
-    }
-    
-    private int size(Node x)
-    {
-        if (x == null) return 0;
-        else return x.size;
-    }
+ 
+     /* Constructor */
+     public LinkedListTree() {
+         root = null;
+     }
+ 
+     /* Function to check if tree is empty */
+     public boolean isEmpty() {
+         return root == null;
+     }
+ 
+     /* Make the tree logically empty */
+     public void clear() {
+         root = null;
+     }
 
-    public Value get(Key key)
-    {
-        return get(root, key);
-    }
-    
-    private Value get(Node x, Key key)
-    {
-        if (x == null) return null;
-        int cmp = key.compareTo(x.key);
-        if (cmp < 0) return get(x.left, key);
-        else if (cmp > 0) return get(x.right, key);
-        else return x.val;
-    }
-    
-    public void put(Key key, Value val)
-    {
-        root = put(root, key, val);
-    }
-    
-    private Node put(Node x, Key key, Value val)
-    {
-        if (x == null) return new Node(key, val, 1);
-        int cmp = key.compareTo(x.key);
-        if (cmp < 0) x.left = put(x.left, key, val);
-        else if (cmp > 0) x.right = put(x.right, key, val);
-        else x.val = val;
-        x.size = size(x.left) + size(x.right) + 1;
-//        if (x.size > 1)
-//        {
-//            // re-balance tree
-//        }
-        return x;
-    }
-    
-    public void delete(Key key)
-    {
-        root = delete(root, key);
-    }
-    
-    private Node delete(Node x, Key key)
-    {
-        if (x == null) return null;
-        int cmp = key.compareTo(x.key);
-        if (cmp < 0) x.left = delete(x.left, key);
-        else if (cmp > 0) x.right = delete(x.right, key);
-        else
-        {
-            if (x.right == null) return x.left;
-            if (x.left == null) return x.right;
-            Node t = x;
-            x = min(t.right);
-            x.right = deleteMin(t.right);
-            x.left = t.left;
+     /* Function to insert data */
+     public void insert(Key key, Value val) {
+         root = insert(key, val, root);
+     }
+
+     /* Function to get height of node */
+     private int height(Node t ) {
+         return t == null ? -1 : t.height;
+     }
+
+     /* Function to max of left/right node */
+     private int max(int lhs, int rhs) {
+         return lhs > rhs ? lhs : rhs;
+     }
+
+     /* Function to insert data recursively */
+     private Node insert(Key key, Value val, Node t) {
+         if (t == null)  t = new Node(key, val, 0);
+         else {
+            int cmp = key.compareTo(t.key);
+            if (cmp < 0) {
+                t.left = insert(key, val, t.left );
+                if (height(t.left) - height(t.right) == 2) {
+                    if (key.compareTo(t.left.key) < 0)
+                        t = rotateWithLeftChild( t );
+                    else
+                        t = doubleWithLeftChild( t );
+                    balanceCnt++;
+                }
+            } else if (cmp > 0) {
+                t.right = insert(key, val, t.right);
+                if (height(t.right) - height(t.left) == 2) {
+                    if (key.compareTo(t.right.key) > 0)
+                        t = rotateWithRightChild( t );
+                    else
+                        t = doubleWithRightChild( t );
+                    balanceCnt++;
+                }
+            }
+         }
+
+         t.height = max(height(t.left), height(t.right)) + 1;
+         return t;
+     }
+     
+     public int GetBalanceCount() {
+         return balanceCnt;
+     }
+
+     /* Rotate binary tree node with left child */     
+     private Node rotateWithLeftChild(Node k2) {
+         Node k1 = k2.left;
+         k2.left = k1.right;
+         k1.right = k2;
+         k2.height = max( height( k2.left ), height( k2.right ) ) + 1;
+         k1.height = max( height( k1.left ), k2.height ) + 1;
+         return k1;
+     }
+ 
+     /* Rotate binary tree node with right child */
+     private Node rotateWithRightChild(Node k1) {
+         Node k2 = k1.right;
+         k1.right = k2.left;
+         k2.left = k1;
+         k1.height = max( height( k1.left ), height( k1.right ) ) + 1;
+         k2.height = max( height( k2.right ), k1.height ) + 1;
+         return k2;
+     }
+
+     /**
+      * Double rotate binary tree node: first left child
+      * with its right child; then node k3 with new left child */
+     private Node doubleWithLeftChild(Node k3) {
+         k3.left = rotateWithRightChild( k3.left );
+         return rotateWithLeftChild( k3 );
+     }
+
+     /**
+      * Double rotate binary tree node: first right child
+      * with its left child; then node k1 with new right child */      
+     private Node doubleWithRightChild(Node k1) {
+         k1.right = rotateWithLeftChild( k1.right );
+         return rotateWithRightChild( k1 );
+     }  
+  
+     /* Functions to count number of nodes */
+     public int countNodes() {
+         return countNodes(root);
+     }
+
+     private int countNodes(Node r) {
+         if (r == null)  return 0;
+         else {
+             int l = 1;
+             l += countNodes(r.left);
+             l += countNodes(r.right);
+             return l;
+         }
+     }
+
+     /* Functions to search for an element */
+     public boolean search(Key key) {
+         return search(root, key);
+     }
+
+     private boolean search(Node r, Key key) {
+         boolean found = false;
+         while ((r != null) && !found) {
+             Key rval = r.key;
+             if (key.compareTo(rval) < 0)
+                r = r.left;
+             else if (key.compareTo(rval) > 0)
+                r = r.right;
+             else {
+                found = true;
+                break;
+            }
+            found = search(r, key);
         }
-        x.size = size(x.left) + size(x.right) + 1;
-        return x;
-    }
-    
-    private Node deleteMin(Node x)
-    {
-        if (x.left == null) return x.right;
-        x.left = deleteMin(x.left);
-        x.size = size(x.left) + size(x.right) + 1;
-        return x;
-    }
-    
-    private Node min(Node x)
-    {
-        if (x.left == null) return x;
-        return min(x.left);
-    }
-    
-    public void print(Node x)
-    {
-        if (x == null) return;
-        print(x.left);
-        System.out.println(x.key);
-        print(x.right);
-    }
-}
+        return found;
+     }
+
+     /* Function for inorder traversal */
+     public void inorder() {
+         inorder(root);
+     }
+
+     private void inorder(Node r) {
+         if (r != null) {
+             inorder(r.left);
+             System.out.print(r.key +" ");
+             inorder(r.right);
+         }
+     }
+
+     /* Function for preorder traversal */
+     public void preorder() {
+         preorder(root);
+     }
+
+     private void preorder(Node r) {
+         if (r != null) {
+             System.out.print(r.key +" ");
+             preorder(r.left);             
+             preorder(r.right);
+         }
+     }
+
+     /* Function for postorder traversal */
+     public void postorder() {
+         postorder(root);
+     }
+
+     private void postorder(Node r) {
+         if (r != null) {
+             postorder(r.left);             
+             postorder(r.right);
+             System.out.print(r.key +" ");
+         }
+     }     
+ }
+ 
